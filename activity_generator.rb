781665295c5912c52800b5a1a41004e0421d716b
Athif Wulandana <athif@wassil.com>
2023-11-30 18:22:45 -0600

    Add create file activity generator

--- activity_generator.rb
+++ activity_generator.rb
@@ -6,6 +6,8 @@ def activity_generator(command, input_array)
   case command
   when "start_process"
     start_process(input_array)
+  when "create_file"
+    create_file(input_array)
   end
 end
 
@@ -14,16 +16,31 @@ def start_process(input_array)
   process_command_line = "#{path} #{args.join(' ')}"
   pid = Process.spawn process_command_line
 
-  start_time = get_start_time(pid)
-  username = get_username(pid)
   process_json = { 
     "pid" => pid,
     "process_command_line" => process_command_line,
-    "start_time" => start_time.strip,
-    "username" => username.strip,
+    "start_time" => get_start_time(pid).strip,
+    "username" => get_username(pid).strip,
     "process_name" => path
   }
   log(process_json)
+end
+
+def create_file(input_array)
+  path = input_array[0]
+  process_command_line = "touch #{path}"
+  pid = Process.spawn process_command_line
+
+  create_file_json = {
+    "pid" => pid,
+    "process_command_line" => process_command_line,
+    "start_time" => get_start_time(pid).strip,
+    "username" => get_username(pid).strip,
+    "filepath" => path,
+    "process_name" => "touch",
+    "activity_descriptor" => "create"
+  }
+  log(create_file_json)
 end
 
 def get_start_time(pid)
